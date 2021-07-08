b763344fe10ad6f69e8325a82e5b24b6539e211a
刘伟峰 <1456622145@qq.com>
2021-07-08 16:06:13 +0800

    Site updated: 2021-07-08 16:06:09

--- js/liuwf.js
+++ js/liuwf.js
@@ -25,3 +25,13 @@ function show_date_time(){
     span_dt_dt.innerHTML=' <font style=color:#2d85f0>'+daysold+'</font> 天 <font style=color:#f4433c>'+hrsold+'</font> 时 <font style=color:#ffbc32>'+minsold+'</font> 分 <font style=color:#0aa858>'+seconds+'</font> 秒';
 }
 show_date_time();
+// 手机侧边栏默认不展开
+var mobile_sidebar_menus = document.getElementById("mobile-sidebar-menus");
+var menus_item_child = mobile_sidebar_menus.getElementsByClassName(
+    "menus_item_child"
+);
+var menus_expand = mobile_sidebar_menus.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+    menus_item_child[i].style.display = "none";
+    menus_expand[i].className += " menus-closed";
+}
